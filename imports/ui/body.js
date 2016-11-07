613451948a1b1b114595497cf927b9d13fc4ef0b
Goncharova Viсtoria <timeontea@gmail.com>
2016-11-07 12:11:35 +0400

    Step 4

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,7 +5,23 @@ import './body.html';
  
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
+    // Show newest tasks at the top
+    return Tasks.find({}, { sort: { createdAt: -1 } });
   },
 });
 
+Template.body.events({
+  'submit .new-task'(event) {
+    event.preventDefault();
+ 
+    const target = event.target;
+    const text = target.text.value;
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+ 
+    target.text.value = '';
+  },
+});
